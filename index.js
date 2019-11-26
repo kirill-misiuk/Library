cdbf9db5cc1d1b8767cba6b7f4f82936b1680d52
KirylMisiuk <kirill.misiuk@gmail.com>
2019-11-26 16:30:12 +0300

    .env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const config = require('./common/config/env.config');
+const dotenv = require ('dotenv');
 const app = express();
-
+dotenv.config ();
 app.listen(config.port, function () {
     console.log('app listening at port %s', config.port);
 });
