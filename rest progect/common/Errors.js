c5c95d90fce40e06755a670448a1c183bcedb85b
KirylMisiuk <kirill.misiuk@gmail.com>
2020-03-03 17:03:02 +0300

    deleted rest

--- rest progect/common/Errors.js
+++
@@ -1,8 +0,0 @@
-class ConflictError extends Error {
-  constructor(bodyID, paramsID) {
-    super();
-    this.name = 'status 400';
-    this.message = `id: ${paramsID} in params and id:${bodyID} in body are not same!`;
-  }
-}
-module.exports = { ConflictError };
--- rest progect/mongoDB/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = async () => {
-  await mongoose.connect('mongodb://127.0.0.1:27017/library', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    .catch((e) => console.log('problem with connection ', e));
-};
